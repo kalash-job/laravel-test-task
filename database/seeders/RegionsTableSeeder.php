f6c5f3cc14b1f136021bd4c1f91f9438c5e0ac4d
Nikolay Kalashnikov <kalash-job@yandex.ru>
2021-02-16 20:09:02 +0300

    Add RegionsTableSeeder

---
+++ database/seeders/RegionsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Region;
+use Illuminate\Database\Seeder;
+
+class RegionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $regions = [
+            'Республика Адыгея',
+            'Республика Алтай',
+            'Республика Башкортостан',
+            'Республика Бурятия',
+            'Республика Дагестан',
+            'Москва',
+        ];
+        foreach ($regions as $regionValue) {
+            $region = new Region();
+            $region->fill(['name' => $regionValue]);
+            $region->save();
+        }
+    }
+}
